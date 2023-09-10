944be0b8f3865f9124087890d9605aeb3a6f8e38
liuqingchao@fenbi.com <liuqingchao@fenbi.com>
2023-09-10 15:20:18 +0800

    dp等部分代码

--- Java/src/main/java/com/yugutou/charpter19_dp/level2/FindLengthOfLCIS.java
+++ Java/src/main/java/com/yugutou/charpter19_dp/level2/FindLengthOfLCIS.java
@@ -1,31 +1,28 @@
 package com.yugutou.charpter19_dp.level2;
 
 /**
- *  LeetCode674.给定一个未经排序的整数数组，
- *  找到最长且连续递增的子序列，并返回该序列的长度。
+ * LeetCode674.给定一个未经排序的整数数组，
+ * 找到最长且连续递增的子序列，并返回该序列的长度。
  */
 public class FindLengthOfLCIS {
     public static void main(String[] args) {
-
+        int[] nums = {1, 3, 5, 4, 7};
+        System.out.println(findLengthOfLCIS(nums));
     }
 
-    public int findLengthOfLCIS(int[] A) {
-        int n = A.length;
-        if (n == 0) {
-            return 0;
-        }
-        int result = 1;
-
-        int[] dp = new int[n];
-        for (int i = 1; i < n; i++) {
+    public static int findLengthOfLCIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        for (int i = 0; i < dp.length; i++) {
             dp[i] = 1;
-            if (A[i] > A[i - 1]) {
-                dp[i] = dp[i - 1] + 1;
-            } else if (A[i] <= A[i - 1]) {
-                dp[i] = 1;  // 也可以不做处理
-            }
-            result = Math.max(result, dp[i]);
         }
-        return result;
+        int res = 1;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i + 1] > nums[i]) {
+                dp[i + 1] = dp[i] + 1;
+            }
+            res = res > dp[i + 1] ? res : dp[i + 1];
+        }
+        return res;
+
     }
 }
--- Java/src/main/java/com/yugutou/charpter3_array/level2/topic2_1删除元素/DeleteDuplicates.java
+++ Java/src/main/java/com/yugutou/charpter3_array/level2/topic2_1删除元素/DeleteDuplicates.java
@@ -5,7 +5,7 @@ package com.yugutou.charpter3_array.level2.topic2_1删除元素;
  */
 public class DeleteDuplicates {
     public static void main(String[] args) {
-        int[] arr = new int[]{1, 2, 3};
+        int[] arr = new int[]{1, 2,3,3,4,4,5 };
         int last = removeDuplicates(arr);
         for (int i = 0; i < last; i++) {
             System.out.print(arr[i] + "  ");
@@ -14,15 +14,16 @@ public class DeleteDuplicates {
 
     public static int removeDuplicates(int[] nums) {
         //slow表示可以放入新元素的位置，索引为0的元素不用管
-        int slow = 1;
+        int slow = 0;
         //循环起到了快指针的作用
         for (int fast = 0; fast < nums.length; fast++) {
-            if (nums[fast] != nums[slow - 1]) {
-                nums[slow] = nums[fast];
+            if (nums[fast] != nums[slow]) {
                 slow++;
+                nums[slow] = nums[fast];
+
             }
         }
-        return slow;
+        return slow+1;
     }
 
     public static int deleteRepeatVal(int[] nums) {
